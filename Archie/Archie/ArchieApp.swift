d5b2760a0921f5a90ef30f250bea79e07bd3dfd7
wrojas9 <wr678@nyu.edu>
2022-10-24 18:39:16 -0400

    First

---
+++ Archie/Archie/ArchieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchieApp.swift
+//  Archie
+//
+//  Created by Will Rojas on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
