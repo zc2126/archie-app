8c25300f6679e766fe7b34961f6ac2f19fe60fae
kairuihuang <6kairuih@gmail.com>
2022-11-07 21:17:28 -0500

    get recommendations based on term

--- Archie/Archie/ContentView.swift
+++ Archie/Archie/ContentView.swift
@@ -10,48 +10,56 @@ import CoreLocation
 import Foundation
 
 var options = ["McDonalds","Wendys","fresh&co","Chipotle","Dig","Shake Shack","Pelicana Chicken"]
-var term = ""
 var out = ""
 var m = DeviceLocationService()
-let apikey = "80aSnHnyHk_OeP8nV1soG9yi6vkMnprpZLNQ75M-wpAKqYgiwgpEXmSToC7MV7d9Wo_PD8pbYMHQ_tLR5lG0qejq8MTZwenFxGWQso6gaHOg3d4xE4gZaKJaCTZXY3Yx"
-//var domainURLString = ""
-let domainURLString = "https://api.yelp.com/v3/businesses/search?location=Greenwich_Village&categories=restaurants&open_now=true"
-
-var restaurants = [Restaurant]()
 
 struct Restaurant: Decodable {
     enum Category: String, Decodable {
         case swift, combine, debugging, xcode
     }
     var name: String
-    //let _ = print(domainURLString)
-    //let url = URL(string: domainURLString)
-    fileprivate func getRest() -> Void{
-        print("url:")
-        print(domainURLString)
+}
+
+var restaurants = [Restaurant]()
+
+struct YelpAPI {
+    let apikey = "80aSnHnyHk_OeP8nV1soG9yi6vkMnprpZLNQ75M-wpAKqYgiwgpEXmSToC7MV7d9Wo_PD8pbYMHQ_tLR5lG0qejq8MTZwenFxGWQso6gaHOg3d4xE4gZaKJaCTZXY3Yx"
+    var domainURLString = "https://api.yelp.com/v3/businesses/search?location=Greenwich_Village&categories=restaurants&open_now=true"
+    let defaultSession = URLSession(configuration: .default)
+    var dataTask: URLSessionDataTask?
+    
+    mutating func setTerm(t: String) -> Void {
+        if t != "" {
+            let underscored_str = t.replacingOccurrences(of: " ", with: "_")
+            domainURLString = "https://api.yelp.com/v3/businesses/search?location=Greenwich_Village&categories=restaurants&open_now=true&term=\(underscored_str)"
+        } else {
+            domainURLString = "https://api.yelp.com/v3/businesses/search?location=Greenwich_Village&categories=restaurants&open_now=true"
+        }
+        self.getRest() // get new set of restaurants based on term
+    }
+    
+    fileprivate func getRest() -> Void {
         let url = URL(string: domainURLString)
         var request = URLRequest(url: url!)
         request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "GET"
-
+        
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 let _ = print("DataTask error: " + error.localizedDescription + "\n")
             }
-            print("pls")
+            restaurants.removeAll() // clear restaurants from previous query
             do {
                 let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
 
                 let _ = print(">>>>", json, #line,"<<<<")
-                
+
                 if let names = json["businesses"] as? [NSDictionary] {
-                    let _ = print("interlude")
                     for r in names {
                         let ro = Restaurant(name: r["name"] as! String)
                             restaurants.append(ro)
                     }
                 }
-                 
             } catch {
                 let _ = print("caught")
             }
@@ -59,12 +67,8 @@ struct Restaurant: Decodable {
     }
 }
 
+var api = YelpAPI()
 
-let defaultSession = URLSession(configuration: .default)
-var dataTask: URLSessionDataTask?
-
-
-let r = Restaurant(name: "")
 /*
 var loc = m.retLoc()
 let lo = loc.location
@@ -75,7 +79,7 @@ let lat = lo?.coordinate.latitude
 struct ContentView: View {
     @State private var t = ""
     @State private var go = false
-    let m = r.getRest()
+    let m = api.getRest()
     var body: some View {
         /*
         let _ = print("here")
@@ -93,21 +97,12 @@ struct ContentView: View {
         ZStack(alignment: .center) {
                 Button("Go") {
                     go.toggle()
-                    //out = restaurants.randomElement()!.name
-
-                    //out = options.randomElement()!
-                    
-                    term = t
-                    print(term)
-                    /*
-                    domainURLString = "https://api.yelp.com/v3/businesses/search?location=Greenwich_Village&categories=restaurants&open_now=true&term=\(term)"
-                    print(domainURLString)
-                     */
-                    
-                    //let r = Restaurant(name: "")
-                    //`r.getRest()
-                     
-                   out = restaurants.randomElement()!.name
+                    api.setTerm(t: t);
+                    if restaurants.count == 0 {
+                        out = "No results"
+                    } else {
+                        out = restaurants.randomElement()!.name
+                    }
                 }.frame(width: 200, height: 100, alignment: .center)
         }
         ZStack(alignment: .bottom) {
@@ -117,8 +112,6 @@ struct ContentView: View {
         }.frame(width: 400, height: 400, alignment: .center)
     }
 }
-
-    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
